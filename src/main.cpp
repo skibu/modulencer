0df17a54a21d4462f980444cb147b52f3de0219c
Michael Smith <mike@goswift.ly>
2025-01-31 20:43:10 -0800

    more tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,46 @@
 #include <thread>
 
 #define DEBUG
+#include "seq/clock.h"
 #include "util/debug.h"
 #include "util/fastRandom.h"
+#include "util/json.hpp"
+
+// Just for testing json
+#include <fstream>
 
 int main() {
-    debug("This is a test of the debug macro\n");
+    debug("This is a test of the debug macro");
 
-    for (int i = 0; i < 20; i++) {
-        std::cout << fast_rand() << std::endl;
+    using json = nlohmann::json;
+    std::ifstream f("example.json");
+
+    try {
+        json data = json::parse(f);
+        float pi = data["pi"];
+        int e = data["answer"]["everything"];
+        auto answer = data["answer"];
+        int every = answer["everything"];
+        int x = 9;
+    } catch (json::parse_error& e) {
+        std::cerr << "JSON parse error: " << e.what() << std::endl;
     }
 
-    for (int i = 0; i < 20; i++) {
-        std::cout << fast_rand(1, 100) << std::endl;
+    for (int i = 0; i < 2; i++) {
+        debug("fast_rand()=%d", fast_rand());
+    }
+
+    for (int i = 0; i < 2; i++) {
+        debug("fast_rand(1, 100)=%d", fast_rand(1, 100));
     }
 
     std::cout << "Hello, World!" << std::endl;
+
+    // Run separate thread forever (for now)
+    auto clock = Clock::create();
+
+    clock.setBPM(60).setPPQN(4).run();
+    clock.join();
+
     return 0;
 }
