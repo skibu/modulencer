72c7b8db2d0c0e9f27d35798643b3b077932686e
Michael Smith <mike@goswift.ly>
2025-01-31 20:44:15 -0800

    Working pretty well
    though still need to get clock time just right.

--- src/seq/clock.cpp
+++ src/seq/clock.cpp
@@ -7,38 +7,88 @@
 #define DEBUG
 #include "../util/debug.h"
 
+Clock Clock::create() {
+    Clock theClock = Clock();
+    theClock.thread = std::thread(&Clock::loop, &theClock);
+    return theClock;
+}
+
 void Clock::run() {
-    std::thread t(loop);
-
-    // Fixme for now wait till thread is done, which means wait forever
-    t.join();
+    debug("Running clock %s...", name.c_str());
+    state = RUNNING;
 }
 
-Clock::Clock() {
-    start = std::chrono::high_resolution_clock::now();
-}
-
-std::chrono::time_point<std::chrono::high_resolution_clock> start;
-
-void Clock::loop() {
-    auto clock = Clock();
-
-    while (true) {
-        std::chrono::duration<float> duration = std::chrono::high_resolution_clock::now() - clock.start;
-
-        debug("Count = %f", duration.count());
-
-        std::cout << duration.count() << std::endl;
-        // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        std::this_thread::sleep_for(std::chrono::microseconds(1000000));
-    }
+void Clock::pause() {
+    debug("Pausing clock %s...", name.c_str());
+    state = PAUSED;
 }
 
 void Clock::reset() {
-    start = std::chrono::high_resolution_clock::now();
+    debug("Resetting clock %s...", name.c_str());
+    bpm_count = 0;
+    ppqn_count = 0;
 }
 
-float Clock::elapsed() {
-    std::chrono::duration<float> duration = std::chrono::high_resolution_clock::now() - start;
-    return duration.count();
+Clock& Clock::setNam2(std::string nam2) {
+    this->nam2 = nam2;
+    return *this;
+}
+
+Clock& Clock::setName(std::string name) {
+    this->name = name;
+    return *this;
+}
+
+Clock& Clock::setBPM(int bpm) {
+    this->bpm = std::clamp(bpm, MIN_BPM, MAX_BPM);
+    return *this;
+}
+
+Clock& Clock::setPPQN(int ppqn) {
+    this->ppqn = std::clamp(ppqn, MIN_PPQN, MAX_PPQN);
+    return *this;
+}
+
+Clock& Clock::addBPMCallback(void (*bpm_callback)(uint32_t, uint32_t)) {
+    bpm_callbacks.push_back(bpm_callback);
+    return *this;
+}
+
+Clock& Clock::addPPQNCallback(void (*ppqn_callback)(uint32_t)) {
+    ppqn_callbacks.push_back(ppqn_callback);
+    return *this;
+}
+
+void Clock::loop() {
+    debug("In loop for clock %s...", name.c_str());
+    while (true) {
+        // Keep track of when PPQN clock tick started
+        using namespace std::chrono;
+        auto start_of_tick = steady_clock::now();
+
+        if (state == RUNNING) {
+            ppqn_count++;
+            debug("Calling ppqn callbacks for ppqn_count=%d", ppqn_count);
+            for (auto callback : ppqn_callbacks)
+                callback(ppqn_count);
+
+            if ((ppqn_count - 1) % ppqn == 0) {
+                bpm_count++;
+                debug("Calling bpm callbacks for bpm_count=%d ppqn_count=%d", bpm_count, ppqn_count);
+                for (auto callback : bpm_callbacks)
+                    callback(bpm_count, ppqn_count);
+            }
+        }
+
+        // Sleep until next PPQN. Some time will have elapsed between the start of the tick and now,
+        // so we need to subtract that from the sleep time
+        duration<float> secs_between_ppqn(60.0f / (ppqn * bpm));
+        auto sleep_time = secs_between_ppqn - (steady_clock::now() - start_of_tick);
+        std::this_thread::sleep_for(sleep_time);
+    }
+}
+
+void Clock::join() {
+    debug("Joining clock %s...", name.c_str());
+    thread.join();
 }
--- src/seq/clock.h
+++ src/seq/clock.h
@@ -2,19 +2,72 @@
 #define CLOCK_H
 
 #include <chrono>
+#include <thread>
 
 class Clock {
    public:
-    static void run();
+    // Creates a new Clock object and starts a new thread
+    static Clock create();
+
+    void run();
+    void pause();
+    void reset();
+
+    std::string nam2  = "Main";  
+    Clock& setNam2(std::string nam2);
+    
+    Clock& setName(std::string name);
+
+    Clock& setBPM(int bpm);
+    int getBPM() {
+        return bpm;
+    }
+
+    Clock& setPPQN(int ppqn);
+    int getPPQN() {
+        return ppqn;
+    }
+
+    Clock& addBPMCallback(void (*bpm_callback)(uint32_t, uint32_t));
+
+    Clock& addPPQNCallback(void (*ppqn_callback)(uint32_t));
+
+    // So that main thread can continue to run while the clock thread is running
+    void join();
 
    protected:
-    Clock();
-    void reset();
-    float elapsed();
-    
+    void loop();
+
    private:
-    static void loop();
-    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    // Constructor is private to force start() to be used instead
+    Clock() {}
+
+    std::thread thread;
+
+    enum State { RUNNING, PAUSED };
+    State state = PAUSED;
+
+    uint32_t bpm_count = 0;
+    uint32_t ppqn_count = 0;
+
+    std::string name = "Main";
+
+    // BPM is Beats Per Minute
+    static inline constexpr int DEFAULT_BPM = 120;
+    static inline constexpr int MIN_BPM = 20;
+    static inline constexpr int MAX_BPM = 300;
+    int bpm = DEFAULT_BPM;
+    std::vector<void (*)(uint32_t, uint32_t)> bpm_callbacks;
+
+    // PPQN is Pulses Per Quarter Note
+    static inline constexpr int DEFAULT_PPQN = 24;
+    static inline constexpr int MIN_PPQN = 1;
+    static inline constexpr int MAX_PPQN = 192;
+    int ppqn = DEFAULT_PPQN;
+    std::vector<void (*)(uint32_t)> ppqn_callbacks;
+
+    static void bpm_callback(uint32_t, uint32_t);
+    static void ppqn_callback(uint32_t);
 };
 
 #endif  // CLOCK_H
